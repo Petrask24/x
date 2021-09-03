7359ccb29787dead7a1897cf2bf416befe1be83a
Petrask24 <petraskie1@gmail.com>
2021-09-03 00:12:04 -0400

    Lots of Changes - Test for Emails
    Contact Us, To Top Arrow, Columns & Rows

--- myJS/myJS.js
+++ myJS/myJS.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
     $(window).scroll(function() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            $("#to-top").slideDown();
+        } else {
+            $("#to-top").slideUp();
+        }
+
         let d = document.documentElement;
         let st = d.scrollTop || $(body).scrollTop;
         let sh = d.scrollHeight || $(body).scrollHeight;
@@ -30,5 +36,29 @@ $(document).ready(function() {
             $("#check").click();
         }
     })
-
+      
+      
 })
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
+
+function submitEmail() {
+    console.log(
+        $(".email").val(),
+        "Email From " + $(".name").val(),
+        $(".message").val()
+    )
+    Email.send({
+        SecureToken : "b88f4d0c-c1d6-4b97-a06c-6f82bf6c8b4a",
+        To : 'chris.petraskie@gmail.com',
+        From : $(".email").val(),
+        Subject : "Email From" + $(".name").val(),
+        Body : $(".message").val()
+    }).then(
+      message => alert(message)
+    );
+}
