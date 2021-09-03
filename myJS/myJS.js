6a9baaaafcf21f96869e557349c1c6bf74a21bf3
Petrask24 <petraskie1@gmail.com>
2021-09-03 01:18:26 -0400

    Final Fix for Email

--- myJS/myJS.js
+++ myJS/myJS.js
@@ -37,7 +37,31 @@ $(document).ready(function() {
         }
     })
       
-      
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    } 
 })
 
 // When the user clicks on the button, scroll to the top of the document
@@ -47,18 +71,16 @@ function topFunction() {
 } 
 
 function submitEmail() {
-    console.log(
-        $(".email").val(),
-        "Email From " + $(".name").val(),
-        $(".message").val()
-    )
+    var modal = document.getElementById("myModal");
+
+    // https://smtpjs.com/
     Email.send({
-        SecureToken : "c0d1ada8-bbb0-412f-a9a4-27219cd14c9b",
+        SecureToken : "70c2b688-f393-47ce-9505-f2e43015f560",
         To : 'chris.petraskie@gmail.com',
         From : $(".email").val(),
-        Subject : "Email From" + $(".name").val(),
+        Subject : "Email From " + $(".name").val(),
         Body : $(".message").val()
     }).then(
-      message => alert(message)
+        modal.style.display = "block"
     );
 }
